0888996a714cc6a9e50268271d8d2e90e0ee2659
AlexJuca <corextechnologies@gmail.com>
2014-12-27 09:34:32 +0100

    Made float-button responsive

--- assets/js/navnav.js
+++ assets/js/navnav.js
@@ -3,12 +3,12 @@ $(document).ready(function()
 	$(window).scroll(function(event){
 
 		var yPos = window.scrollY; 
-		if(yPos > 400)
+		if(yPos > 600)
 		{
 			$('#nav-profile').css('opacity','1').css('transition','all 0.3s ease-in-out');
 		}
-		
-		if(yPos < 400)
+
+		if(yPos < 600)
 		{
 			$('#nav-profile').css('opacity','0').css('transition','all 0.3s ease-in-out');
 		}
